0fc7ae5a0f0ed6ecaa633b94f8e044a941d3f44a
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-05-21 14:37:47 -0600

    Added function to count num of each size of tshirt

--- tshirtSorter.js
+++ tshirtSorter.js
@@ -9,3 +9,17 @@ const tshirtSort = string => {
 }
 
 console.log(tshirtSort('slsmmsllsmsmlmsls'));
+console.log(tshirtSort('smlsmllsmslsmlmslsmslmslmmlssmlssmlllllmsmmslsmslmlsmsmslmsmmlsl'));
+console.log("\n----------------------------------\n");
+
+//additional functionality for large numbers of tshirts
+const numTshirts = string => {
+    let sorted = tshirtSort(string);
+    let small = Array.prototype.filter.call(sorted, char => char === 's').length;
+    let medium = Array.prototype.filter.call(sorted, char => char === 'm').length;
+    let large = Array.prototype.filter.call(sorted, char => char === 'l').length;
+    return `You need ${small} small, ${medium} medium, and ${large} large tshirts.`;
+}
+
+console.log(tshirtSort("smllmlmlmsslmmlmlmlmssssmlmssmslmslmslsmlsmslmslmslmslsmlsmlsmmslmmslsmlsmlsllslmlmslslllsmslmslmsssmslmlsmslml"));
+console.log(numTshirts("smllmlmlmsslmmlmlmlmssssmlmssmslmslmslsmlsmslmslmslmslsmlsmlsmmslmmslsmlsmlsllslmlmslslllsmslmslmsssmslmlsmslml"));
